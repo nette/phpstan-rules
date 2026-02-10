abb04bfac8841b83f7b22298a0eec0685dd530ce
David Grudl <david@grudl.com>
2026-02-10 13:18:49 +0100

    added boilerplate

---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+// The Nette Tester command-line runner can be
+// invoked through the command: ../vendor/bin/tester .
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer install`';
+	exit(1);
+}
+
+
+// configure environment
+Tester\Environment::setup();
+Tester\Environment::setupFunctions();
+date_default_timezone_set('Europe/Prague');
